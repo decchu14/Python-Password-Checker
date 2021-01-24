e6949dba6b91c4b1a4c2811f4c12bda2c19d683c
decchu14 <1decchu14@gmail.com>
2021-01-24 11:17:22 +0530

    adding steps as comments

--- Console_PswdChecker.py
+++ Console_PswdChecker.py
@@ -4,15 +4,16 @@ import hashlib
 import sys
 
 '''
-    step 9 - receiving first five characters of our hashed password.
+    step 9 - receiving first 5 characters of our hashed password.
     step 10 - assingning the api url and hashed password
-    step 11 - requesting and fetching all the hashed passowrds from api which matches our hashed password and assigning   
-              it to a variable 'res'. 
+    step 11 - requesting and fetching all the tails of a hashed passowrds from api whose 1st 5 chars matches our hashed  
+              password's 1st 5 chars and assigning it to a variable 'res'.
     step 12 - checking if the returned result from api gives status code 200
             - 200 status code indicates that the request has succeeded.
-    step 13 - if status code of result is not 200 then the following error will be raised.
-    step 14 - returning res which contains all the hashed passwords followed by :count to pwnd_api_check function.
-            - for example :- CBFDAC6008F9CAB4083784CBD1874F76618D2A97:100
+    step 13 - if status code of result is not 200 then the error will be raised.
+    step 14 - returning res which contains all the tails of a hashed passwords followed by :count to pwnd_api_check 
+              function.
+            - for example :- CBFDAC6008F9CAB4083784CBD1874F76618D2A97:100 [tail:count]
             - here 100 will be the count of how many times a password has been pwnd.
 '''
 
@@ -27,12 +28,12 @@ def requests_api_data(query_char):  # 9
 
 
 '''
-    step 16 - hashes : all the response hashes
+    step 16 - hashes : all the tails of a response hashes
             - hash_to_check : tail hash of our password
     step 17 - as mentioned in the 14th step the response hashes will in the form of    
-              CBFDAC6008F9CAB4083784CBD1874F76618D2A97:100 
+              CBFDAC6008F9CAB4083784CBD1874F76618D2A97:100 [tail:count]
             - we need to split the hash from count 
-            - line.split(':') the seperator based on which we split iis ':'.
+            - line.split(':')is the method. the seperator based on which we split is ':'.
     step 18,19,20,21 - loop through splited hash(h) and count one by one and if hash(h) matches to hash_to_check(tail) 
                        then return the count else return 0 to pwnd_api_ckeck function. 
 '''
@@ -53,12 +54,12 @@ def get_password_leaks_count(hashes, hash_to_check):  # 16
            - hexdigest() : Returns the encoded data in hexadecimal format.
            - upper() : converts tha hashed password to uppercase.
     step 7 - here the hashed passowrd will be divided into 2 parts, that is first 5 characters into one part and rest
-             character into second part.
-    step 8 - passing the first 5 characters of our hash to the requests_api_data function and assigning it to variable   
-             named 'response'.
-    step 15 - passing the tail of our password and the all response hashes which we fetched from password api to 
+             character sinto second part.
+    step 8 - passing the first 5 characters of our hashed password to the requests_api_data function and assigning the    
+             returned result to variable named 'response'.
+    step 15 - passing the tail of our password and the all tail response hashes which we fetched from password api to 
               get_password_leaks_count function.
-            - returning it to the function main function
+            - returning it to the main function
 '''
 
 
